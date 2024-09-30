1395d06dbc28088628a35db9cb99e9cc4c8fd50a
florence <loracodes3@gmail.com>
2024-09-30 19:06:27 +0300

    nested if

---
+++ index.js
@@ -0,0 +1,19 @@
+const age = 17;
+
+let isAdult, canWork, canEnlist, canDrink;
+
+if (age >= 16) {
+  canWork = true;
+
+  if (age >= 18) {
+    isAdult = true;
+    canEnlist = true;
+
+    if (age >= 21) {
+      canDrink = true;
+    }
+  }
+}
+
+canWork;
+console.log(canWork)
